7057f5be9050f30f85b4b3cc549479bbb54eb3e1
Francesco Demarco <francescodem402@gmail.com>
2025-03-25 23:18:55 -0400

    Fixed appostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-         Hey What's Up!
+         {"Hey What's Up!"}
+
       </div>
     </main>
   );
